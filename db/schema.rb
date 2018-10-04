b201d047169044754c4a9c2fa70d87273a9cee6b
Andrew Kairys <andrewkairys@gmail.com>
2018-10-04 17:05:19 -0400

    add omniauth  params to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180930140958) do
+ActiveRecord::Schema.define(version: 20181004181653) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20180930140958) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20180930140958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
