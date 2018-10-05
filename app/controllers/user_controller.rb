192713e04d55308c6f637c8c69b854de0a9acba4
Andrew Kairys <andrewkairys@gmail.com>
2018-10-05 09:43:19 -0400

    removing actions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,42 +1,47 @@
-class UserController < ApplicationController
-
+ class UserController < ApplicationController
+#   before_action :set_product, only: [:show, :edit, :update, :destroy]
+#   before_action :authenticate_user!
   #Set all products for the current_user
-  def index
-    @user = current_user
-    @products = Product.all.select{|p| p.user_id == @user.id}
-  end
+#   def index
+#     @user = current_user
+#     @products = Product.all.select{|p| p.user_id == @user.id}
+#   end
 
-  def show 
-    @users = current_user
-  end
+#   def show 
+#     @products = current_user.products
+#   end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+#   def edit
+#     @product = Product.find(params[:id])
+#   end
 
-  def new
-    @product = current_user.products.build
-  end
+#   def new
+#     @product = current_user.products.build
+#   end
 
-  def create
-    @product = current_user.products.build(product_params)
+#   def create
+#     @product = current_user.products.build(product_params)
 
-    if @product.save
-      redirect_to root_path
-    else
-      :abort
-    end
-  end
+#     if @product.save
+#       redirect_to root_path
+#     else
+#       :abort
+#     end
+#   end
 
-  def destroy
-    @product.destroy
-    redirect_to root_path
-  end
+#   def destroy
+#     @product.destroy
+#     redirect_to category_products_path
+#   end
 
-  private
+#   private
 
-  def product_params
-    params.require(:product).permit(:name, :description, :price, {:category_ids => []})
-  end
+#   def set_product
+#     @product = Product.find(params[:id])
+#   end
 
-end
+#   def product_params
+#     params.require(:product).permit(:name, :description, :price, {:category_ids => []})
+#   end
+
+ end
